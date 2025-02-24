c0d9935a487748d692512349a488d29b30cc62dc
xpakx <55586470+xpakx@users.noreply.github.com>
2025-02-24 20:32:20 +0100

    test: add basic test of wasm initialization

---
+++ tests/example.test.ts
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+interface NimrodWasmInputs {
+    init: (width: number, height: number) => number;
+    updateSize: (width: number, height: number) => number;
+    tick: () => void;
+    sendImage: (ptr: number, width: number, height: number) => void;
+    malloc: (size: number) => number;
+    onMouseClick: (button: number, mouseX: number, mouseY: number) => void;
+    onMouseMove: (mouseX: number, mouseY: number) => void;
+    onMouseUp: (button: number, mouseX: number, mouseY: number) => void;
+    onMouseWheel: (deltaY: number) => void;
+    onKeyDown: (key: number) => void;
+    onKeyUp: (key: number) => void;
+}
+
+interface NimrodWasmOutputs {
+    jsprintf: (base: any) => void;
+    drawCanvas: (ptr: number, length: number) => void;
+}
+
+interface Wasi {
+    fd_close: () => void;
+    fd_seek: () => void;
+    fd_write: () => void;
+}
+
+test('Wasm initialized correctly', async () => {
+	let wasi: Wasi = {
+		fd_close: function() { },
+		fd_seek: function() { },
+		fd_write: function() { }
+	};
+
+	let io_wasm: NimrodWasmOutputs = {
+		jsprintf: function(_base: any) {},
+		drawCanvas: function(_ptr: number, _length: number) {}
+	};
+
+	const wasmPath = path.resolve(__dirname, '../dist/web.wasm');
+	const memory = new WebAssembly.Memory({ initial: 2 });
+	const buffer = fs.readFileSync(wasmPath);
+	// @ts-expect-error: wasm 
+	const { instance } = await WebAssembly.instantiate(buffer, { env: { memory }, wasi_snapshot_preview1: wasi, io_wasm: io_wasm });
+	const nimrod = instance.exports as unknown as NimrodWasmInputs;
+
+	expect(nimrod.init(10, 10)).toBe(0);
+});
